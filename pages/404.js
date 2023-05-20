95bb2ff4d1c8be528b5f28a5d1d23931156d75cb
Mahmoud samy <msamy13579@gmail.com>
2023-05-21 01:45:29 +0300

    fix notfound

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import {useEffect} from "react";
 import {useRouter} from "next/router";
-import {Head} from "next/document";
+import Head from "next/head";
 
 const NotFound = () => {
     const router = useRouter();
